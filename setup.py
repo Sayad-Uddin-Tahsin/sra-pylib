1922a2f197a58c81c552e2afbf774b42ef08647d
Sayad Uddin Tahsin <89304780+Sayad-Uddin-Tahsin@users.noreply.github.com>
2023-03-06 18:03:11 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='sraPy',
+    name='sra-Py',
     version='1.0.0',
     author='Sayad Uddin Tahsin',
     author_email='mr.pluto012@gmail.com',
     description='A Wrapper of some-random-api for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/Sayad-Uddin-Tahsin/sraPy',
+    url='https://github.com/Sayad-Uddin-Tahsin/sra-Py',
     packages=find_packages(),
     install_requires=[
         'requests>=2.28.2'
     ],
     dependency_links=[
-        'https://pypi.org/project/sraPy/'
+        'https://pypi.org/project/sra-Py/'
     ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
