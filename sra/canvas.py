defdc4f14fd407d2719e67f6444719c6eb22d745
Sayad Uddin Tahsin <89304780+Sayad-Uddin-Tahsin@users.noreply.github.com>
2023-04-10 15:28:47 +0600

    Update canvas.py
    Signed-off-by: Sayad Uddin Tahsin <89304780+Sayad-Uddin-Tahsin@users.noreply.github.com>

--- sra/canvas.py
+++ sra/canvas.py
@@ -8,7 +8,7 @@ import re
 def is_valid_url(url):
     pattern = re.compile(
         r'^https?://'
-        r'(?:[a-z0-9]+(?:\.[a-z0-9]+)*/)*' # updated pattern to avoid backtracking
+        r'(?:[a-z0-9]+(?:\.[a-z0-9]+)*/)*'
         r'[a-z0-9]+\.[a-z]+/?$'
     )
     return bool(pattern.match(url))
